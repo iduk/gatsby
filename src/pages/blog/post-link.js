27ecf8a9d396bcccadc09847a678c74c7824e661
Duk <iduk.design@gmail.com>
2022-08-29 21:43:54 +0900

    Update post-link.js

--- src/pages/blog/post-link.js
+++ src/pages/blog/post-link.js
@@ -3,10 +3,10 @@ import { Link } from 'gatsby'
 
 const PostLink = ({ post }) => (
   <div>
-    <Link to={`/blog/${post.frontmatter.slug}`}>
+    <Link to={`${post.frontmatter.slug}`}>
       <h1>{post.frontmatter.title}</h1>
       <p>{post.frontmatter.date}</p>
-      <p>{post.excerpt}</p>
+      <p>{post.frontmatter.excerpt}</p>
     </Link>
   </div>
 )
